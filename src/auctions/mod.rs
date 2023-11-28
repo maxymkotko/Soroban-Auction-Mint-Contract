a7b8a44d71a761773bd830bf783b4bf879df927d
maxymkotko <maxkotko@outlook.com>
2023-11-29 02:24:57 +0900

    Improve modules documentation

--- src/auctions/mod.rs
+++ src/auctions/mod.rs
@@ -6,6 +6,19 @@
     MIT License
 */
 
+//! The `auctions` module implements a time-based auction system using trait-based polymorphism
+//! and enum-based dispatch (strategy design pattern) to allow modular extension
+//! for auction behaviors.
+//!
+//! Implemented features:
+//! 
+//! - Descending price auctions (see: behavior_descending_price.rs) supporting linear 
+//!   or compound discount, and customizable frequency/rate.
+//! - Ascending price auctions (see: behavior_ascending_price.rs) with "buy now" option.
+//! - Reserve price.
+//! - Anti-snipe mechanism.
+//! - Concurrent and cancellable bids.
+
 pub mod behavior;
 pub mod behavior_descending_price;
 pub mod behavior_ascending_price;
